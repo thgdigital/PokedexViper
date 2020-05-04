fac237ccbd0c556ece21d5e188f0c0f248dcd0d7
Thiago Santos <>
2020-05-04 18:33:43 -0300

    [ADD] Criando module home

---
+++ PokedexViper/Modules/Home/HomeWireframe.swift
@@ -0,0 +1,24 @@
+//
+//  HomeWireframe.swift
+//  PokedexViper
+//
+//  Created by Thiago Vaz on 03/05/20.
+//  Copyright © 2020 Thiago Vaz. All rights reserved.
+//
+
+import UIKit
+
+class HomeWireframe {
+    
+    static func make(window: UIWindow) -> HomeListView {
+        let viewController = HomeListView()
+        let navigation = UINavigationController(rootViewController: viewController)
+        let navigationBar  = navigation.navigationBar
+        navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationBar.shadowImage = UIImage()
+        navigationBar.isTranslucent = true
+        window.rootViewController = navigation
+        window.makeKeyAndVisible()
+        return viewController
+    }
+}
